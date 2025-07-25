bb7642ab25a5b2bdb0db4cd9e532ec4905dc9238
Fernando Aquino Terrazas <fernandoaquinoterrazas@MacBook-Air-de-Fernando.local>
2025-07-24 22:42:28 -0600

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { USUARIOS_FALSOS } from './fake-users';
 @Component({
   selector: 'app-raiz',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
