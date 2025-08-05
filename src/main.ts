0eb59124a0205881cd6744dc61f0dab56fffdb58
Fernando Aquino Terrazas <fernandoaquinoterrazas@MacBook-Air-de-Fernando.local>
2025-08-04 21:28:26 -0600

    config global app localization

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localesEs from '@angular/common/locales/es';
+import { LOCALE_ID } from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+registerLocaleData(localesEs, 'es');
+
+bootstrapApplication(AppComponent, {
+    providers:[
+        {provide:LOCALE_ID, useValue: 'es'},
+    ],
+}).catch((err) => console.error(err));
