492d9dac9cf9eef107726a2b41107385f84c7adc
Fernando Aquino Terrazas <fernandoaquinoterrazas@MacBook-Air-de-Fernando.local>
2025-08-06 20:37:06 -0600

    Final class for this project fix localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
@@ -8,13 +8,10 @@ import { SharedModule } from './shared/shared.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
   imports: [BrowserModule, SharedModule, TasksModule], //la propiedad imports es para componentes de tipo standalone
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
 })
 export class AppModule {}
 //Browser module solo de debe importar en el modulo raíz de la aplicacion, es decir solo una vez en toda la aplicación.
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,9 @@
 
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
+import { registerLocaleData } from '@angular/common';
+import localesEs from '@angular/common/locales/es';
 
-// import { AppComponent } from './app/app.component';
-// import { registerLocaleData } from '@angular/common';
-// import localesEs from '@angular/common/locales/es';
-// import { LOCALE_ID } from '@angular/core';
-
-// registerLocaleData(localesEs, 'es');
-
-// bootstrapApplication(AppComponent, {
-//     providers:[
-//         {provide:LOCALE_ID, useValue: 'es'},
-//     ],
-// }).catch((err) => console.error(err));
+registerLocaleData(localesEs, 'es');
 
 platformBrowserDynamic().bootstrapModule(AppModule); // bootstrapmodule se le conoce como el modulo raíz.
